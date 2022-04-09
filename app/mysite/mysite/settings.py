2db13cdcdc6ea073562df1f1052e4ad6ee70d69c
Steve Cross <sdc.stevex@gmail.com>
2022-04-09 05:54:12 +0000

    allow all hostts

--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@fpl*pe$-l=mv8!(tf-*+nzc5@qa&4yb_yl$npu2#_=s!(8*b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
